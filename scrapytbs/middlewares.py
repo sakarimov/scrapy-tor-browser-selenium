d3e8d41f31a3e5c5af2747ab3a5746b1c7494712
sulthankarimov <sulthankarimov@gmail.com>
2024-05-24 10:21:52 +0700

    implement headless as default

--- scrapytbs/middlewares.py
+++ scrapytbs/middlewares.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 from .http import tbsRequest
 
+from tbselenium import tbdriver
 from tbselenium.tbdriver import TorBrowserDriver
 
 
@@ -17,17 +18,40 @@ class tbsAddon:
 class tbsMiddleware:
     """Scrapy middleware handling the requests using tor-browser-selenium"""
 
-    def __init__(self, driver_name):
+    def __init__(
+            self,
+            driver_name,
+            browser_executable_path,
+            driver_arguments,
+            headless
+    ):
+        driver_options = getattr(tbdriver, "Options")
+        # TODO implement driver_arguments
+        # for argument in driver_arguments:
+        #    driver_options.add_argument(argument)
+
         self.driver = TorBrowserDriver(
-            '/home/sulthan/.local/opt/tor-browser/app/')
+            browser_executable_path,
+            headless=headless
+        )
 
     @classmethod
     def from_crawler(cls, crawler):
         """Initialize the middleware with the crawler settings"""
         driver_name = 'tor-browser'
 
+        browser_executable_path = crawler.settings.get(
+            'TBS_BROWSER_EXECUTABLE_PATH')
+
+        driver_arguments = crawler.settings.get('TBS_DRIVER_ARGUMENTS')
+
+        headless = crawler.settings.get('TBS_DRIVER_HEADLESS')
+
         middleware = cls(
             driver_name=driver_name,
+            browser_executable_path=browser_executable_path,
+            driver_arguments=driver_arguments,
+            headless=headless
         )
 
         crawler.signals.connect(
@@ -38,7 +62,7 @@ class tbsMiddleware:
     def process_request(self, request, spider):
         """Process a request using the tor-browser-selenium driver if applicable"""
 
-        if not isinstance(request, stbRequest):
+        if not isinstance(request, tbsRequest):
             return None
 
         self.driver.get(request.url)
