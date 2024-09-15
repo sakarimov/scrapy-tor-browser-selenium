5832e43d50c2bc5ae332b2929bcf45e982414998
sulthankarimov <sulthankarimov@gmail.com>
2024-09-15 15:43:00 +0700

    add more control to scrapytbs

--- scrapytbs/torProxyMiddleware.py
+++ scrapytbs/torProxyMiddleware.py
@@ -15,18 +15,40 @@ def new_tor_identity(password):
 
 
 class torProxyMiddleware(HttpProxyMiddleware):
+
     def process_response(self, request, response, spider):
         # Get a new identity depending on the response
         password = get_project_settings()['TBS_TOR_PASSWORD']
-        if response.status != 200:
+
+        identity_renewal_rate = get_project_settings(
+        )['TBS_IDENTITY_RENEWAL_RATE']
+        req = identity_renewal_rate
+
+        per_error_renewal = get_project_settings(
+        )['TBS_PER_ERROR_RENEWAL']
+
+        if req > 0:
+            req -= 1
+        else:
             new_tor_identity(password)
+            req = identity_renewal_rate
+            print('new tor identity')
+            return request
+        if response.status != 200 and per_error_renewal:
+            new_tor_identity(password)
+            print('new tor identity')
             return request
         return response
 
     def process_request(self, request, spider):
         # Set the Proxy
         # A new identity for each request
-        # Comment out if you want to get a new Identity only through process_response
+        # if TBS_PER_REQUEST_RENEWAL set to True
         password = get_project_settings()['TBS_TOR_PASSWORD']
-        new_tor_identity(password)
+
+        per_request_renewal = get_project_settings(
+        )['TBS_PER_REQUEST_RENEWAL']
+
+        if per_request_renewal:
+            new_tor_identity(password)
         request.meta['proxy'] = 'http://127.0.0.1:8118'
